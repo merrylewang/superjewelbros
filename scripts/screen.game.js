4e34615470be634d64bfa7a2ddc9467035f3a3a8
Merryle <merrylew@gmail.com>
2017-11-11 15:20:34 -0800

    currently playable

--- scripts/screen.game.js
+++ scripts/screen.game.js
@@ -1,19 +1,113 @@
 jewel.screens["game-screen"] = (function() {
     var firstRun = true,
-        paused;
+        paused,
+        cursor;
 
     function startGame() {
         var board = jewel.board,
             display = jewel.display;
         board.initialize(function() {
             display.initialize(function() {
+              cursor = {x:0,y:0,selected:false};
                 display.redraw(board.getBoard(), function() {
                     // do nothing for now
                 });
             });
         });
     }
-    
+
+    function setCursor(x,y,select){
+      cursor.x = x;
+      cursor.y = y;
+      cursor.selected = select;
+      jewel.display.setCursor(x,y,select);
+    }
+
+    function selectJewel(x, y) {
+        if (arguments.length === 0) {
+            selectJewel(cursor.x, cursor.y);
+            return;
+        }
+        if (cursor.selected) {
+            var dx = Math.abs(x - cursor.x),
+                dy = Math.abs(y - cursor.y),
+                dist = dx + dy;
+
+            if (dist === 0) {
+                // deselected the selected jewel
+                setCursor(x, y, false);
+            } else if (dist == 1) {
+                // selected an adjacent jewel
+                jewel.board.swap(cursor.x, cursor.y,
+                    x, y, playBoardEvents);
+                setCursor(x, y, false);
+            } else {
+                // selected a different jewel
+             setCursor(x, y, true);
+            }
+        } else {
+            setCursor(x, y, true);
+        }
+    }
+    function playBoardEvents(events) {
+       var display = jewel.display;
+       if (events.length > 0) {
+           var boardEvent = events.shift(),
+               next = function() {
+                   playBoardEvents(events);
+               };
+           switch (boardEvent.type) {
+               case "move" :
+                   display.moveJewels(boardEvent.data, next);
+             break;
+               case "remove" :
+                   display.removeJewels(boardEvent.data, next);
+                   break;
+               case "refill" :
+                   display.refill(boardEvent.data, next);
+                   break;
+               default :
+                   next();
+                   break;
+           }
+       } else {
+           display.redraw(jewel.board.getBoard(), function() {
+               // good to go again
+           });
+       }
+   }
+
+   function moveCursor(x,y){
+     var settings = jewel.settings;
+     if(cursor.selected){
+       x += cursor.x;
+       y += cursor.y;
+       if(x >= 0 && x < settings.cols && y >= 0 && y < settings.rows){
+         selectJewel(x,y);
+       }
+     }else{
+       x = (cursor.x + x +settings.cols) % settings.cols;
+       y = (cursor.y + y + settings.rows) % settings.rows;
+       setCursor(x,y,false);
+     }
+   }
+
+   function moveUp(){
+     moveCursor(0,-1);
+   }
+
+   function moveDown(){
+     moveCursor(0,1);
+   }
+
+   function moveLeft(){
+     moveCursor(-1,0);
+   }
+
+   function moveRight(){
+     mvoeCursor(1,0);
+   }
+
     function pauseGame() {
         if (paused) {
             return; // do nothing if already paused
@@ -30,7 +124,7 @@ jewel.screens["game-screen"] = (function() {
         overlay.style.display = "none";
         paused = false;
     }
-    
+
     function exitGame() {
         pauseGame();
         var confirmed = window.confirm(
@@ -47,6 +141,13 @@ jewel.screens["game-screen"] = (function() {
         dom.bind("footer button.exit", "click", exitGame);
         dom.bind("footer button.pause", "click", pauseGame);
         dom.bind(".pause-overlay", "click", resumeGame);
+        var input = jewel.input;
+        input.initialize();
+        input.bind("selectJewel", selectJewel);
+        input.bind("moveUp", moveUp);
+        input.bind("moveDown", moveDown);
+        input.bind("moveLeft", moveLeft);
+        input.bind("moveRight", moveRight);
     }
 
     function run() {
